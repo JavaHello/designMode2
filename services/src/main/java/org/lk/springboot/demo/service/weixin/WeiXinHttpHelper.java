a6ee5535ef37848666826bfb7bc31e2d66ff11fc
27238 <27238@192.168.0.107>
2017-10-11 23:55:17 +0800

    代码整理

--- services/src/main/java/org/lk/springboot/demo/service/weixin/WeiXinHttpHelper.java
+++ services/src/main/java/org/lk/springboot/demo/service/weixin/WeiXinHttpHelper.java
@@ -1,29 +1,10 @@
 package org.lk.springboot.demo.service.weixin;
 
-import org.apache.commons.io.IOUtils;
-import org.apache.commons.lang3.StringUtils;
-import org.apache.http.HttpEntity;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.BufferedHttpEntity;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.util.EntityUtils;
+import java.io.IOException;
+
 import org.lk.springboot.demo.util.HttpClientHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
-import java.util.List;
 
 @Service
 public class WeiXinHttpHelper {
