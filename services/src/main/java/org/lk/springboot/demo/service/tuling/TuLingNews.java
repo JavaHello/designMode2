7dc51159ad9b5fe3b64167dd5fed0fb156cca58f
27238 <27238@192.168.0.109>
2017-10-14 10:34:22 +0800

    代码整理

--- services/src/main/java/org/lk/springboot/demo/service/tuling/TuLingNews.java
+++
@@ -1,6 +0,0 @@
-package org.lk.springboot.demo.service.tuling;
-
-public class TuLingNews {
-    private String article;
-    private String source;
-}
--- web/src/main/java/org/lk/springboot/demo/web/controller/IndexController.java
+++ web/src/main/java/org/lk/springboot/demo/web/controller/IndexController.java
@@ -1,12 +1,33 @@
 package org.lk.springboot.demo.web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
+
+import com.alibaba.fastjson.JSONObject;
 
 @Controller
 public class IndexController {
 
-    @RequestMapping("/login")
+	private final RestTemplate restTemplate;
+	
+	
+	@Autowired
+    public IndexController(RestTemplate restTemplate) {
+		super();
+		this.restTemplate = restTemplate;
+	}
+	@RequestMapping("/")
+	@ResponseBody
+    public Object test(){
+		String url = "http://localhost:8888";
+		JSONObject forObject = restTemplate.getForObject(url , JSONObject.class);
+        return forObject;
+    }
+    
+	@RequestMapping("/login")
     public String login(){
         return "login";
     }
